f05b57b8daa74c3511538dee0902e76c3116ae8e
Pawel Wirth <pawelwirth33@gmail.com>
2023-07-13 19:22:04 +0200

    poprawa

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let fristNumber= 1
 let secondNumber= 2
  function  multiply (num1 ,num2 ) {
-  let result = num1 * num2 
+  let result = num1 + num2 
   return result;
  }
 
  multiply(1, 2);
- console.log (multiply(1, 2));
+ console.log (multiply(fristNumber,secondNumber));
  
